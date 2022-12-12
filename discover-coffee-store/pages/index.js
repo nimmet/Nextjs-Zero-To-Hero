f56319f15ffe202873c372ad380d6225d5ece825
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2022-12-12 21:23:44 +0300

    Discover Coffee Store

---
+++ discover-coffee-store/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className=' text-center text-lg font-bold text-green-300 italic'>
+      <h1>Coffee Store</h1>
+    </div>
+  )
+}
