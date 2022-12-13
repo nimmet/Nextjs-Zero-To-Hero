9475887babb89047a9b234b56dbbcda39f69691f
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2022-12-13 21:22:36 +0300

    added footer to app page

--- discover-coffee-store/pages/_app.js
+++ discover-coffee-store/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(<div className='flex flex-col justify-center items-center'>
+
+   <Component {...pageProps} /> {" "}
+   <footer className='
+   max-w-screen-md absolute bottom-0 text-center'>
+    <p>© 2022 Uyghur</p>
+   </footer>
+  </div>)
 }
 
 export default MyApp
